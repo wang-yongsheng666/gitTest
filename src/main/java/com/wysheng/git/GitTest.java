47d55a2b7a2b4c5dfeae4da21d8f99022978ef21
WangYongsheng <1925818135@qq.com>
2022-07-09 09:19:15 +0800

    hot-fix first commit

--- src/main/java/com/wysheng/git/GitTest.java
+++ src/main/java/com/wysheng/git/GitTest.java
@@ -3,7 +3,7 @@ package com.wysheng.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git");
-
+        System.out.println("Hello");
     }
 
     public void swap(int a, int b) {
