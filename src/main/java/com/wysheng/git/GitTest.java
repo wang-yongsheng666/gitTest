58d9f594f3d35a85b3c3141dfa1dfdf41352461d
WangYongsheng <1925818135@qq.com>
2022-07-09 09:00:32 +0800

    second commit

--- src/main/java/com/wysheng/git/GitTest.java
+++ src/main/java/com/wysheng/git/GitTest.java
@@ -3,5 +3,12 @@ package com.wysheng.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git");
+
     }
+
+    public void swap(int a, int b) {
+
+    }
+
+
 }
