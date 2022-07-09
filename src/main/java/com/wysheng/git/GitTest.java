49c4a188622534ef78abf7f721c5c2a440f8add3
WangYongsheng <1925818135@qq.com>
2022-07-09 09:02:09 +0800

    third commit

--- src/main/java/com/wysheng/git/GitTest.java
+++ src/main/java/com/wysheng/git/GitTest.java
@@ -7,8 +7,12 @@ public class GitTest {
     }
 
     public void swap(int a, int b) {
-
+        int temp = a;
+        a = b;
+        b = temp;
     }
 
-
+    public int max(int x, int y) {
+        return x > y ? x : y;
+    }
 }
